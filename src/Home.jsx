74fa711a39670be7966604265d7691c1d1e7da28
rinaroy456 <reenaroy456@gmail.com>
2024-04-15 19:30:29 +0545

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h3>My name is rina roy </h3>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <h1>Home page </h1>
+    },
+    {
+        path:"/about ",
+        element : <h1>About page </h1>
+    }
+])
+
+export default router;
